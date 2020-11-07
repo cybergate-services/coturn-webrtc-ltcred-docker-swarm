1d62464d20d4901f21dc8837fca892b4a79678ce
Chinthaka Deshapriya <35320965+chinthakadeshapriya@users.noreply.github.com>
2020-11-07 17:57:45 +0530

    Create gen-coturn-user-password-from-secret.sh

---
+++ gen-coturn-user-password-from-secret.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+secret=cybersecret && \
+time=$(date +%s) && \
+expiry=8400 && \
+username=$(( $time + $expiry )) &&\
+echo username:$username && \
+echo password : $(echo -n $username | openssl dgst -binary -sha1 -hmac $secret | openssl base64)
